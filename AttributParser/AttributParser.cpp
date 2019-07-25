1c83b1c4732ea1d9e964204044867c8539021ce8
Estellise <shiranoyukihime@gmail.com>
2019-07-25 11:00:31 +0800

    attribute parser success

--- AttributParser/AttributParser.cpp
+++ AttributParser/AttributParser.cpp
@@ -14,7 +14,6 @@ void find_tag(std::string* currentTag, std::string str)
 		currentTag->append(".");
 	}
 
-	std::string tag = "";
 
 	for (int i = 0; i < str.length(); i++) 
 	{
@@ -25,19 +24,30 @@ void find_tag(std::string* currentTag, std::string str)
 		}
 
 
-		if (str[i] != '<' && str[i] != ' ') 
+		if (str[i] != '<' && str[i] != '>')
 		{
-			tag += str[i];
+			*currentTag += str[i];
 		}
 		else if (str[i] == '<' && str[i + 1] == '/') 
 		{
-			currentTag->clear();
+
+			currentTag->erase(currentTag->length() - 1);
+
+			for (int x = currentTag->size() - 1; x > -1; --x) 
+			{
+				if ((*currentTag)[x] == '.') 
+				{		
+					currentTag->erase(x);
+
+					break;
+				}
+				currentTag->erase(x);
+			}
+
 			break;
 		}
 
 	}
-
-	(*currentTag) = tag;
 
 }
 
@@ -46,7 +56,6 @@ void attr_value(const std::string tag, std::map<std::string, std::string>* hrml,
 
 	
 	std::string currentTag = tag;
-	std::string attrNameLabel = "";
 	std::string attrValueLabel = "";
 
 	currentTag += '~';
@@ -55,17 +64,22 @@ void attr_value(const std::string tag, std::map<std::string, std::string>* hrml,
 	bool attrValueBool = false;
 
 
-
-	for (int i = (currentTag).length(); i < str.length(); i++) 
+	for (int i = 2; i < str.length(); i++) 
 	{
-	
-		if(attrNameBool)
+
+		if (attrNameBool)
 		{
-			attrNameLabel += str[i];
+			if (str[i] != ' ' && str[i] != '=' && str[i] != '"') 
+			{
+				currentTag += str[i];
+			}
 		}
 		else if (attrValueBool)
 		{
-			attrValueLabel += str[i];
+			if (str[i] != +'"') 
+			{
+				attrValueLabel += str[i];
+			}
 		}
 
 
@@ -73,31 +87,26 @@ void attr_value(const std::string tag, std::map<std::string, std::string>* hrml,
 		{
 			attrNameBool = true;
 		}
-		else if((str[i] == ' ' || str[i] == '=') && attrNameBool)
-		{
-			attrNameBool = false;
-			
-			attrNameLabel.erase(attrNameLabel.length() - 1);
-			currentTag += attrNameLabel;
-		}
-
 
 		if (str[i] == '"') 
 		{
 
 			if (attrValueBool)
 			{
-				attrValueLabel.erase(attrValueLabel.length() - 1);
-
-				hrml->insert(attrNameLabel, attrValueLabel);
+				
+				hrml->insert(std::pair<std::string, std::string>(currentTag, attrValueLabel));
 
 				currentTag = tag +  '~';
-				attrNameLabel = "";
 				attrValueLabel = "";
 				
 			}
 
 			attrValueBool = !attrValueBool;
+
+			if (attrValueBool) 
+			{
+				attrNameBool = false;
+			}
 
 		}
 	}
@@ -115,14 +124,52 @@ int main()
 
 	std::map<std::string, std::string> hrml;
 
-	std::cin >> N;
-	std::cin >> Q;
+	//std::cin >> N;
+	//std::cin >> Q;
 
-	for (int i = 0; i < N; i++) 
+	std::vector<std::string> nT = { "<tag1 v1 = \"123\" v2 = \"43.4\" v3 = \"hello\">",
+									"</tag1>",
+									"<tag2 v4 = \"v2\" name = \"Tag2\">",
+									"<tag3 v1 = \"Hello\" v2 = \"World!\">",
+									"</tag3>",
+									"<tag4 v1 = \"Hello\" v2 = \"Universe!\">",
+									"</tag4>",
+									"</tag2>",
+									"<tag5>",
+									"<tag7 new_val = \"New\">",
+									"</tag7>",
+									"</tag5>",
+									"<tag6>",
+									"<tag8 intval = \"34\" floatval = \"9.845\">",
+									"</tag8>",
+									"</tag6>"};
+
+	std::vector<std::string> qT = { "tag1~v1",
+									"tag1~v2",
+									"tag1~v3",
+									"tag4~v2",
+									"tag2.tag4~v1",
+									"tag2.tag4~v2",
+									"tag2.tag3~v2",
+									"tag5.tag7~new_val",
+									"tag5~new_val",
+									"tag7~new_val",
+									"tag6.tag8~intval",
+									"tag6.tag8~floatval",
+									"tag6.tag8~val",
+									"tag8~intval" };
+	
+	//std::cin >> std::ws;
+
+	for (int i = 0; i < /** /N/**//**/nT.size()/**/; i++) 
 	{
 		std::string temp;
 
-		std::cin >> temp;
+		//
+		//std::getline(std::cin, temp);
+
+		//test
+		temp = nT[i];
 
 		find_tag(&tag, temp);
 
@@ -130,15 +177,18 @@ int main()
 		{
 			attr_value(tag, &hrml, temp);
 		}
-		
+
 	}
 	
 
-	for (int i = 0; i < Q; i++) 
+	for (int i = 0; i < /** /Q/**//**/qT.size()/**/; i++) 
 	{
 		std::string temp;
 
-		std::cin >> temp;
+		//std::cin >> temp;
+
+		//test
+		temp = qT[i];
 
 		if (!temp.empty()) 
 		{
@@ -148,11 +198,21 @@ int main()
 			// if exist output value
 
 			// if not output Not Exist!
+			auto it = hrml.find(temp);
+
+			if (it != hrml.end()) 
+			{
+				std::cout << it->second << std::endl;
+			}
+			else 
+			{
+				std::cout << "Not Exists" << std::endl;
+			}
 		}
 	}
 
 
-
+	std::cout << "<<";
 
 
 	return 0;
